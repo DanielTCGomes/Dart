6f6d9364e1a13459d81ab6fce7553d155da4f8a7
Daniel Teixeira <danielcrispim.teixeira@gmail.com>
2022-05-13 01:25:07 -0300

    Update for_1dart

--- controle/for_1.dart
+++ controle/for_1.dart
@@ -2,10 +2,14 @@ main() {
   for (int a = 100; a >= 0; a--) {
     print('a = $a');
   }
+  //Extrutura de controle for , basicamente a expressão funciona assim neste caso acima :
+  //ele executa o 100 verifica se é maior ou igual a 0 é verdadeiro então ele segue para
+  // a segunda execução : na segunda execução ele irá executar o a-- antes de fazer a verificação
+  //ou seja agr ele vai verificar se 99 é maior ou igual a zero e assim por diante até que seja falso
+  // até que não seja maior ou igual a zero
+  //realizando uma decrescente de 100 a 0.
 
   print('FIM!');
-
-  //De 100 a 0 decrescente
 
   for (int a = 0; a <= 100; a++) {
     print('a = $a');
