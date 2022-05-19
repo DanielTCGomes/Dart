a6db35cd8f2ea06812e0d326abc75d28df781a38
Daniel Teixeira <danielcrispim.teixeira@gmail.com>
2022-05-19 03:22:24 -0300

    updatecontrole3
    updatecontrole3

---
+++ controle/desafio_for.dart
@@ -0,0 +1,20 @@
+//#
+//##
+//###
+//####
+//#####
+//######
+// Use o laço FOR,
+//controle o laço sem usar um valor númerico!
+// e depois usando valor númerico!
+main() {
+  for (var valor = '#'; valor != '#######'; valor += '#') {
+    print(valor);
+  }
+
+  var valor1 = '#';
+  for (var a = 1; a <= 6; a++) {
+    print(valor1);
+    valor1 += '#';
+  }
+}
